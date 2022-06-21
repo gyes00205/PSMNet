38f5dac63896e87a91fdc3b87af97b63a1ab81a9
gyes00205 <gyes00205@gmail.com>
2022-06-21 15:44:31 +0800

    Fix identation error

--- Test_img.py
+++ Test_img.py
@@ -58,75 +58,71 @@ if args.loadmodel is not None:
 
 print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
 
+
 def test(imgL,imgR):
-        model.eval()
+    model.eval()
 
-        if args.cuda:
-           imgL = imgL.cuda()
-           imgR = imgR.cuda()     
+    if args.cuda:
+        imgL = imgL.cuda()
+        imgR = imgR.cuda()     
 
-        with torch.no_grad():
-            disp = model(imgL,imgR)
+    with torch.no_grad():
+        disp = model(imgL,imgR)
 
-        disp = torch.squeeze(disp)
-        pred_disp = disp.data.cpu().numpy()
+    disp = torch.squeeze(disp)
+    pred_disp = disp.data.cpu().numpy()
 
-        return pred_disp
+    return pred_disp
 
 
 def main():
 
-        normal_mean_var = {'mean': [0.485, 0.456, 0.406],
-                            'std': [0.229, 0.224, 0.225]}
-        infer_transform = transforms.Compose([transforms.ToTensor(),
-                                              transforms.Normalize(**normal_mean_var)])    
+    normal_mean_var = {'mean': [0.485, 0.456, 0.406],
+                        'std': [0.229, 0.224, 0.225]}
+    infer_transform = transforms.Compose([transforms.ToTensor(),
+                                            transforms.Normalize(**normal_mean_var)])    
 
-        imgL_o = Image.open(args.leftimg).convert('RGB')
-        imgR_o = Image.open(args.rightimg).convert('RGB')
+    imgL_o = Image.open(args.leftimg).convert('RGB')
+    imgR_o = Image.open(args.rightimg).convert('RGB')
 
-        imgL = infer_transform(imgL_o)
-        imgR = infer_transform(imgR_o) 
-       
+    imgL = infer_transform(imgL_o)
+    imgR = infer_transform(imgR_o) 
+    
 
-        # pad to width and hight to 16 times
-        if imgL.shape[1] % 16 != 0:
-            times = imgL.shape[1]//16       
-            top_pad = (times+1)*16 -imgL.shape[1]
-        else:
-            top_pad = 0
+    # pad to width and hight to 16 times
+    if imgL.shape[1] % 16 != 0:
+        times = imgL.shape[1]//16       
+        top_pad = (times+1)*16 -imgL.shape[1]
+    else:
+        top_pad = 0
 
-        if imgL.shape[2] % 16 != 0:
-            times = imgL.shape[2]//16                       
-            right_pad = (times+1)*16-imgL.shape[2]
-        else:
-            right_pad = 0    
+    if imgL.shape[2] % 16 != 0:
+        times = imgL.shape[2]//16                       
+        right_pad = (times+1)*16-imgL.shape[2]
+    else:
+        right_pad = 0    
 
-        imgL = F.pad(imgL,(0,right_pad, top_pad,0)).unsqueeze(0)
-        imgR = F.pad(imgR,(0,right_pad, top_pad,0)).unsqueeze(0)
+    imgL = F.pad(imgL,(0,right_pad, top_pad,0)).unsqueeze(0)
+    imgR = F.pad(imgR,(0,right_pad, top_pad,0)).unsqueeze(0)
 
-        start_time = time.time()
-        pred_disp = test(imgL,imgR)
-        print('time = %.2f' %(time.time() - start_time))
+    start_time = time.time()
+    pred_disp = test(imgL,imgR)
+    print('time = %.2f' %(time.time() - start_time))
 
-        
-        if top_pad !=0 and right_pad != 0:
-            img = pred_disp[top_pad:,:-right_pad]
-        elif top_pad ==0 and right_pad != 0:
-            img = pred_disp[:,:-right_pad]
-        elif top_pad !=0 and right_pad == 0:
-            img = pred_disp[top_pad:,:]
-        else:
-            img = pred_disp
-        
-        img = (img*256).astype('uint16')
-        img = Image.fromarray(img)
-        img.save('Test_disparity.png')
+    
+    if top_pad !=0 and right_pad != 0:
+        img = pred_disp[top_pad:,:-right_pad]
+    elif top_pad ==0 and right_pad != 0:
+        img = pred_disp[:,:-right_pad]
+    elif top_pad !=0 and right_pad == 0:
+        img = pred_disp[top_pad:,:]
+    else:
+        img = pred_disp
+    
+    img = (img*256).astype('uint16')
+    img = Image.fromarray(img)
+    img.save('Test_disparity.png')
+
 
 if __name__ == '__main__':
    main()
-
-
-
-
-
-
